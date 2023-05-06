e8fb6a95e3abee5fea4f1a4fe96834874e8c979f
Marcos Candeia <marrcooos@gmail.com>
2023-05-06 20:16:09 -0300

    Add rotate keys command
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- security/keys.ts
+++ security/keys.ts
@@ -3,24 +3,29 @@ export const hash = "SHA-256";
 
 const PUBLIC_KEY_ENV_VAR = "WORKERS_PUBLIC_KEY";
 const PRIVATE_KEY_ENV_VAR = "WORKERS_PRIVATE_KEY";
+
+const generateKeyPair = async () => {
+  const keyPair = await crypto.subtle.generateKey(
+    {
+      name: alg,
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash,
+    },
+    true,
+    ["sign", "verify"],
+  );
+  return await Promise.all([
+    crypto.subtle.exportKey("jwk", keyPair.publicKey),
+    crypto.subtle.exportKey("jwk", keyPair.privateKey),
+  ]);
+};
+
 const getOrGenerateKeyPair = async (): Promise<[JsonWebKey, JsonWebKey]> => {
   const publicKeyEnvValue = Deno.env.get(PUBLIC_KEY_ENV_VAR);
   const privateKeyEnvValue = Deno.env.get(PRIVATE_KEY_ENV_VAR);
   if (!publicKeyEnvValue || !privateKeyEnvValue) {
-    const keyPair = await crypto.subtle.generateKey(
-      {
-        name: alg,
-        modulusLength: 2048,
-        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-        hash,
-      },
-      true,
-      ["sign", "verify"],
-    );
-    return await Promise.all([
-      crypto.subtle.exportKey("jwk", keyPair.publicKey),
-      crypto.subtle.exportKey("jwk", keyPair.privateKey),
-    ]);
+    return await generateKeyPair();
   }
   return [
     JSON.parse(atob(publicKeyEnvValue)),
@@ -29,3 +34,20 @@ const getOrGenerateKeyPair = async (): Promise<[JsonWebKey, JsonWebKey]> => {
 };
 // Generate an RSA key pair
 export const [publicKey, privateKey] = await getOrGenerateKeyPair();
+
+// rotate keys if necessary
+if (import.meta.main) {
+  const [kpub, kprivate] = await generateKeyPair();
+  const pubKeyB64 = btoa(JSON.stringify(kpub));
+  const privaKeyB64 = btoa(JSON.stringify(kprivate));
+  const command = new Deno.Command("flyctl", {
+    args: [
+      "secrets",
+      "set",
+      `${PUBLIC_KEY_ENV_VAR}=${pubKeyB64}`,
+      `${PRIVATE_KEY_ENV_VAR}=${privaKeyB64}`,
+    ],
+  });
+  const process = command.spawn();
+  await process.status;
+}
